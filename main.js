20b908a23de842bc65530ea538c0891da08d1074
~joeljamestech <162905644+joeljamestech2@users.noreply.github.com>
2026-01-17 23:50:03 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static folders
+app.use("/style", express.static(path.join(__dirname, "style")));
+app.use("/scripts", express.static(path.join(__dirname, "scripts")));
+app.use("/icons", express.static(path.join(__dirname, "icons")));
+
+// Serve index.html
+app.get("/joel-xmd-yt-downloader", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
