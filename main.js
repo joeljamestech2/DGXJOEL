250ea61adb1944ebd8f24e23bff181d1992caa29
~joeljamestech <162905644+joeljamestech2@users.noreply.github.com>
2026-01-18 00:13:30 +0300

    DgXjoel

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ app.use("/scripts", express.static(path.join(__dirname, "scripts")));
 app.use("/icons", express.static(path.join(__dirname, "icons")));
 
 // Serve index.html
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/joel-xmd-youtube-media-downloader", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
