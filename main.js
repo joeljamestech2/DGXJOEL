809c8781fb213ca5a6cf1223705c2b304ea35809
~joeljamestech <162905644+joeljamestech2@users.noreply.github.com>
2026-01-18 00:15:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.use("/scripts", express.static(path.join(__dirname, "scripts")));
 app.use("/icons", express.static(path.join(__dirname, "icons")));
 
 // Serve index.html
-app.get("/joel-xmd-youtube-media-downloader", (req, res) => {
+app.get("/youtube", (req, res) => {
   res.sendFile(path.join(__dirname, "src/index.html"));
 });
 
